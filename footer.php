91221d52d2cbec6f812cb8c3928215015ac37148
wwrk22 <wwrk22@gmail.com>
2020-09-04 01:28:31 -0400

    Added spacing and indentation

--- footer.php
+++ footer.php
@@ -12,6 +12,7 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+        
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'happy-christmas-portfolio' ) ); ?>">
 				<?php
@@ -24,9 +25,9 @@
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'happy-christmas-portfolio' ), 'happy-christmas-portfolio', '<a href="https://thewongalaxy.com">Won Rhim</a>' );
 				?>
-		</div><!-- .site-info -->
+        </div><!-- .site-info -->
+        
 	</footer><!-- #colophon -->
-</div><!-- #page -->
 
 <?php wp_footer(); ?>
 
--- header.php
+++ header.php
@@ -11,49 +11,63 @@
 
 ?>
 <!doctype html>
+
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
-</head>
+    <head>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'happy-christmas-portfolio' ); ?></a>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$happy_christmas_portfolio_description = get_bloginfo( 'description', 'display' );
-			if ( $happy_christmas_portfolio_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $happy_christmas_portfolio_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+        <?php wp_head(); ?>
+        
+    </head>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'happy-christmas-portfolio' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+    <body <?php body_class(); ?>>
+
+        <?php wp_body_open(); ?>
+
+        <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'happy-christmas-portfolio' ); ?></a>
+
+        <header id="masthead" class="site-header">
+
+            <div class="site-branding">
+    
+                <?php the_custom_logo(); ?>
+
+                <?php esc_attr_e( 'Horizontal', 'happy-christmas-portfolio' ); ?>
+            
+                <?php if ( is_front_page() && is_home() ) : ?>
+                    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <?php else : ?>
+                    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                <?php endif; ?>
+
+                
+                <?php
+                $happy_christmas_portfolio_description = get_bloginfo( 'description', 'display' );
+
+                if ( $happy_christmas_portfolio_description || is_customize_preview() ) : ?>
+                    <p class="site-description"><?php echo $happy_christmas_portfolio_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+                <?php endif; ?>
+
+            </div><!-- .site-branding -->
+
+            <nav id="site-navigation" class="main-navigation">
+                
+                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'happy-christmas-portfolio' ); ?></button>
+
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'menu-1',
+                        'menu_id'        => 'primary-menu',
+                    )
+                );
+                ?>
+                
+            </nav><!-- #site-navigation -->
+
+        </header><!-- #masthead -->
